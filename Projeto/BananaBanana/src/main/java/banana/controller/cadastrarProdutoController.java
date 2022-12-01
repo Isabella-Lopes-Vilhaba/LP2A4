7f34259f9bb16b6de82df6552fd71c52c5ece88e
Isabella Lopes Vilhaba <isabellavilhaba@gmail.com>
2022-11-30 23:22:20 -0300

    Finaliza projeto

---
+++ Projeto/BananaBanana/src/main/java/banana/controller/cadastrarProdutoController.java
@@ -0,0 +1,74 @@
+package banana.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import banana.model.Produto;
+
+/**
+ * Servlet implementation class cadastrarProdutoController
+ */
+public class cadastrarProdutoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public cadastrarProdutoController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset-UTF-8");
+		
+		String descricao = request.getParameter("descricao");
+		boolean online = false;
+		
+		String mensagem;
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("cadastrarProduto.jsp");
+		
+		if(
+			descricao != null && !descricao.isEmpty()
+			&& request.getParameter("quantidade") != null && !request.getParameter("quantidade").isEmpty()
+			&& request.getParameter("preco") != null && !request.getParameter("preco").isEmpty()
+		) {
+			int quantidade;
+			quantidade = Integer.parseInt(request.getParameter("quantidade"));
+
+			double preco;
+			preco = Double.parseDouble(request.getParameter("preco"));
+
+			if(request.getParameter("online")!= null && request.getParameter("online").equals("on")) {
+				online = true;
+			};
+			
+			Produto produto = new Produto(descricao, quantidade, preco, online);
+			produto.salvar();
+
+			mensagem = "Produto cadastrado com sucesso!";
+		} else {
+			mensagem = "Os campos precisam ser preenchidos";
+		}
+		
+		request.setAttribute("mensagem", mensagem);
+		dispatcher.forward(request, response);
+	}
+
+}
